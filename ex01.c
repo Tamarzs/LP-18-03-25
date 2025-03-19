1613c1694e76ddc481e42930089b53497ca12687
Tamarozi <cauaptamarozi@gmail.com>
2025-03-18 21:27:21 -0300

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int main() {
+    float nota1;
+    float nota2;
+    float nota3;
+    
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+    
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+    
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+    
+    float media = (nota1 + nota2 + nota3) / 3;
+    printf("Média: %.1f", media);
+    return 0;
+}
